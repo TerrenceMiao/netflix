bdd1103fc89ce00ff758472f2f8f079285f1b20e
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-06-24 21:10:26 +1000

    Init Discovery service imports

---
+++ discovery/src/main/java/org/paradise/netflix/spring/DiscoveryApplication.java
@@ -0,0 +1,17 @@
+package org.paradise.netflix.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DiscoveryApplication.class, args);
+	}
+
+}
