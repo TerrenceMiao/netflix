9ce30f990349b9fd2401d7beef0f6cf3faf24061
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2016-06-26 20:24:40 +1000

    Add Spring Boot Admin which doesn't work with Eureka

---
+++ spring-boot-admin/src/main/java/org/paradise/netflix/spring/AdminApplication.java
@@ -0,0 +1,20 @@
+package org.paradise.netflix.spring;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableAdminServer
+public class AdminApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AdminApplication.class, args);
+	}
+
+}
---
+++ spring-boot-admin/src/test/java/org/paradise/netflix/spring/AdminApplicationTests.java
@@ -0,0 +1,18 @@
+package org.paradise.netflix.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AdminApplication.class)
+@WebIntegrationTest
+public class AdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
